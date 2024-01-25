ef59c941e0e82b115ad8d7881370c0fe14b1fb71
Tsvetelin Tsetskov <tsvetelin.tsetskov@halodx.com>
2024-01-25 12:08:16 +0200

    Updated database script

--- config/init.sql
+++ config/init.sql
@@ -1,56 +1,116 @@
-create database if not exists clipboard;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 25, 2024 at 11:06 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
-use clipboard;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-create table if not exists `user`(
-    id          int unique not null auto_increment,
-    email       text unique not null,
-    username    text unique not null,
-    password    text not null,
-    is_admin    boolean not null default false,
 
-    -- Auditing fields
-    created_at timestamp not null default now(),
-    last_updated_at timestamp not null default now(),
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-    -- Soft deletion
-    is_deleted boolean not null default false,
+--
+-- Database: `clipboard`
+--
+CREATE DATABASE IF NOT EXISTS `clipboard` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `clipboard`;
 
-    constraint pk_user primary key (id)
-);
+-- --------------------------------------------------------
 
-create table if not exists auth_token(
-    id int unique not null auto_increment,
-    user_id int not null,
-    token varchar(255) not null,
-    expires_at timestamp not null,
-    
-    primary key (id),
-    foreign key (user_id) references user(id)
-);
+--
+-- Table structure for table `auth_token`
+--
 
--- Semantic - subscriber is following the user
-create table if not exists subscription(
-	subscriber_id int not null,
-	user_id int not null,
-    created_at timestamp not null default now(),
+CREATE TABLE IF NOT EXISTS `auth_token` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `expires_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-	primary key (subscriber_id, user_id)
-);
+-- --------------------------------------------------------
 
-create table if not exists clip(
-    id int unique not null auto_increment,
-    name varchar(255) not null,
-    description text,
-    resource_type varchar(63) not null,
-    resource_data text,
-    is_public boolean not null default true,
-    owner_id int not null,
+--
+-- Table structure for table `clip`
+--
 
-    uploaded_at timestamp not null default now(),
-    last_updated_at timestamp not null default now(),
-    
-    primary key (id),
-    foreign key (owner_id) references user(id)
-);
+CREATE TABLE IF NOT EXISTS `clip` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL,
+  `resource_type` varchar(63) NOT NULL,
+  `resource_data` text NOT NULL DEFAULT '""',
+  `is_public` tinyint(1) NOT NULL DEFAULT 1,
+  `owner_id` int(11) NOT NULL,
+  `uploaded_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `last_updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `owner_id` (`owner_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subscription`
+--
+
+CREATE TABLE IF NOT EXISTS `subscription` (
+  `subscriber_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`subscriber_id`,`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` text NOT NULL,
+  `username` text NOT NULL,
+  `password` text NOT NULL,
+  `is_admin` tinyint(1) NOT NULL DEFAULT 0,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `last_updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `is_deleted` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `email` (`email`) USING HASH,
+  UNIQUE KEY `username` (`username`) USING HASH
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `auth_token`
+--
+ALTER TABLE `auth_token`
+  ADD CONSTRAINT `auth_token_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
+-- Constraints for table `clip`
+--
+ALTER TABLE `clip`
+  ADD CONSTRAINT `clip_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `user` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
