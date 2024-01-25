b78383e480d0704c0e1f4cfacc0baff8610adf07
Elizabethvk <beeetiiis@gmail.com>
2024-01-25 22:14:12 +0200

    design changes in myclips, addclip, footer,header

--- src/add_clip/add_clip.php
+++ src/add_clip/add_clip.php
@@ -19,21 +19,24 @@ if (!isset($_SESSION['logged_in'])) {
 <head>
     <meta charset="utf-8">
     <title>Добави отрязък</title>
+    <link rel="stylesheet" type="text/css" href="./add_clip.css">
     <link rel="icon" type="image/png" sizes="32x32" href="../../img/favicon.png">
 </head>
 
 <body>
-    <p id="user-message">
-        <?php
-        if (isset($_SESSION['msg'])) {
-            echo $_SESSION['msg'];
-        }
-        unset($_SESSION['msg']);
-        ?>
-    </p>
+
     <form id="add-clip-form" class="add-clip-form" action="./add_clip_action.php" method="post"
         enctype="multipart/form-data">
+
         <h1>Добави отрязък</h1>
+        <p id="user-message">
+            <?php
+            if (isset($_SESSION['msg'])) {
+                echo $_SESSION['msg'];
+            }
+            unset($_SESSION['msg']);
+            ?>
+        </p>
 
         <input placeholder="Име" name="name" required>
         <input placeholder="Описание" name="description">
@@ -49,10 +52,17 @@ if (!isset($_SESSION['logged_in'])) {
             <option value="other">Other</option>
         </select>
 
-        <input placeholder="Твоят отрязък тук" name="clip_content" required>
+        <!-- <div id="clip_content"> -->
+        <input placeholder="Твоят отрязък тук" name="clip_content" id="clip-content" required>
+        <!-- </div> -->
 
-        <label for="myCheckbox">Личен</label>
-        <input type="checkbox" name="private">
+        <div class="checkbox-container">
+            <label for="myCheckbox" id="privacy">Личен</label>
+            <input type="checkbox" name="private">
+        </div>
+
+        <!-- <label for="myCheckbox" id="privacy">Личен</label> -->
+        <!-- <input type="checkbox" name="private"> -->
         <!-- TODO Tsvetelin : input file -->
 
         <button class="btn" id="save-button" type="submit" name="save">Качи</button>
--- src/my_clips/my_clips.php
+++ src/my_clips/my_clips.php
@@ -19,87 +19,90 @@ if (!isset($_SESSION['logged_in'])) {
 <head>
     <meta charset="utf-8">
     <title>Моите отрезки</title>
+    <link rel="stylesheet" type="text/css" href="./my_clips.css">
     <link rel="icon" type="image/png" sizes="32x32" href="../../img/favicon.png">
 </head>
 
 <body>
-    <p id="user-message">
-        <?php
-        if (isset($_SESSION['msg'])) {
-            echo $_SESSION['msg'];
-        }
-        unset($_SESSION['msg']);
-        ?>
-    </p>
-    <h1>Моите отрезки</h1>
-    <table>
-        <thead>
-            <th>Име</th>
-            <th>Описание</th>
-            <th>Тип</th>
-            <th>Съдържание</th>
-            <th>Частни</th>
-            <th>Действия</th>
-        </thead>
-        <?php
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            return;
-        }
-        require_once $_SERVER['DOCUMENT_ROOT'] . '/src/Db.php';
-        require_once $_SERVER['DOCUMENT_ROOT'] . '/src/transformers.php';
-
-        function printValidActionsForClip($clip)
-        {
-            $transformableTo = array_map(function ($transformer) {
-                return $transformer->canTransformTo();
-            }, availableTransformersFor($clip['resource_type']));
-
-            echo '<select id="transformer_select" name="transformer_select">';
-            foreach ($transformableTo as $idx => $transformableToStr) {
-
-                echo "<option value=\"$idx\">$transformableToStr</option>";
+    <main class="section" id="main">
+        <p id="user-message">
+            <?php
+            if (isset($_SESSION['msg'])) {
+                echo $_SESSION['msg'];
             }
-            echo '</select>';
-        }
+            unset($_SESSION['msg']);
+            ?>
+        </p>
+        <h1>Моите отрезки</h1>
+        <table class="clip-table">
+            <thead>
+                <th>Име</th>
+                <th>Описание</th>
+                <th>Тип</th>
+                <th>Съдържание</th>
+                <th>Частни</th>
+                <th>Действия</th>
+            </thead>
+            <?php
+            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                return;
+            }
+            require_once $_SERVER['DOCUMENT_ROOT'] . '/src/Db.php';
+            require_once $_SERVER['DOCUMENT_ROOT'] . '/src/transformers.php';
 
-        $clips = $db->getSnippetsForUser($_SESSION['user_id']);
-        foreach ($clips as $idx => $clip) {
-            echo '<form class="list-clip-run-form" action="./transform_clip_action.php" method="post"
+            function printValidActionsForClip($clip)
+            {
+                $transformableTo = array_map(function ($transformer) {
+                    return $transformer->canTransformTo();
+                }, availableTransformersFor($clip['resource_type']));
+
+                echo '<select id="transformer_select" name="transformer_select">';
+                foreach ($transformableTo as $idx => $transformableToStr) {
+
+                    echo "<option value=\"$idx\">$transformableToStr</option>";
+                }
+                echo '</select>';
+            }
+
+            $clips = $db->getSnippetsForUser($_SESSION['user_id']);
+            foreach ($clips as $idx => $clip) {
+                echo '<form class="list-clip-run-form" action="./transform_clip_action.php" method="post"
             enctype="multipart/form-data">';
 
-            echo "<tr>
+                echo "<tr>
             <td>$clip[name]</td>
             <td>$clip[description]</td>
             <td>$clip[resource_type]</td>";
 
-            echo "<td>";
-            if (strlen($clip["resource_data"]) <= 50) {
-                echo htmlspecialchars("$clip[resource_data]");
-            } else {
-                echo "Съдържанието на ресурса е твърде дълго.";
-            }
-            echo "</td>";
+                echo "<td>";
+                if (strlen($clip["resource_data"]) <= 50) {
+                    echo htmlspecialchars("$clip[resource_data]");
+                } else {
+                    echo "Съдържанието на ресурса е твърде дълго.";
+                }
+                echo "</td>";
 
-            echo "<td>";
-            if (!$clip["is_public"]) {
-                echo "V";
-            } else {
-                echo "X";
-            }
-            echo "</td>";
+                echo "<td id=\"privacy\">";
+                if (!$clip["is_public"]) {
+                    echo "✓";
+                } else {
+                    echo "X";
+                }
+                echo "</td>";
 
-            echo "<td>";
-            printValidActionsForClip($clip);
-            echo "</td>";
-            echo "<td>";
-            echo "<button type=\"submit\" name=\"act\" value=\"$idx\">Act</button>";
-            echo "</td>";
-            echo "</tr>";
-            echo "</form>";
-            // TODO Tsvetelin : edit clips
-        }
-        ?>
-    </table>
+                echo "<td>";
+                printValidActionsForClip($clip);
+                echo "</td>";
+                echo "<td>";
+                echo "<button type=\"submit\" name=\"act\" value=\"$idx\">Изпълни</button>";
+                echo "</td>";
+                echo "</tr>";
+                echo "</form>";
+                // TODO Tsvetelin : edit clips
+            }
+            ?>
+        </table>
+    </main>
 </body>
 
 </html>
