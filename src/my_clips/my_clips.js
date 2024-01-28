be1e2d363b1d28c69922e1dfe806526b58c59028
Elizabethvk <beeetiiis@gmail.com>
2024-01-28 23:17:02 +0200

    added success message to the share link

--- src/my_clips/my_clips.js
+++ src/my_clips/my_clips.js
@@ -1,3 +1,13 @@
+// function copyShareLink(clipId) {
+//     navigator.clipboard.writeText(window.location.origin + "/src/view_clip/view_clip.php?clipId=" + clipId);
+// }
+
 function copyShareLink(clipId) {
-    navigator.clipboard.writeText(window.location.origin + "/src/view_clip/view_clip.php?clipId=" + clipId);
+    const shareLink = window.location.origin + "/src/view_clip/view_clip.php?clipId=" + clipId;
+    navigator.clipboard.writeText(shareLink).then(function () {
+        const userMessage = document.getElementById("user-message");
+        userMessage.innerText = "Линкът за споделяне е копиран в клипборда!";
+    }).catch(function (err) {
+        console.error('Проблем с копирането на линка в клипборда!', err);
+    });
 }
